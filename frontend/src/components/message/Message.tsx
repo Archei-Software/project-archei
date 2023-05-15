346c1e4bf16b44a75e3ae7322a6d162ec2892f6f
Victor Alves <victormssalves@outlook.com>
2023-05-15 16:58:02 -0300

    sex

--- frontend/src/components/message/Message.tsx
+++ frontend/src/components/message/Message.tsx
@@ -1,6 +1,34 @@
-import {AiOutlineMail} from 'react-icons/ai'
+import {AiOutlineMail} from 'react-icons/ai';
+import { useState } from 'react';
+import emailjs from '@emailjs/browser';
+import img from '../../assets/imgs/image.png'
+
 
 const Message: React.FC = () => {
+const [name, setName] = useState('');
+const [email, setEmail] = useState('');
+const [message, setMessage] = useState('');
+//const [image = {img}] = useState('');
+
+
+  function sendEmail (e: any) {
+    e.preventDefault()
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email,
+      img
+    }
+    console.log(templateParams)
+    emailjs.send("service_qb7vvtg", "template_ftk18x6", templateParams, "Vfrdy_j6MLcNUX3aE").then((response) => {
+      console.log('Email enviado', response.status, response.text)
+      setName('');
+      setEmail('');
+      setMessage('');
+    }, (err) => {
+      console.log('ERRO:', err.text)
+    })
+  }
   return (
     <>
       <section className="text-gray-600 body-font relative">
@@ -18,11 +46,38 @@ const Message: React.FC = () => {
           ></iframe>
         </div>
         <div className="container px-5 py-24 mx-auto flex ">
-          <div className="lg:w-1/3 md:w-1/2 bg-white dark:bg-[#101010] rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+          <form onSubmit={sendEmail} className="lg:w-1/3 md:w-1/2 bg-white dark:bg-[#101010] rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
             <h2 className="text-black dark:text-white text-2xl mb-1 font-bold flex align-middle justify-center">
             <AiOutlineMail className="mt-1 mr-1"/>Entre em contato
             </h2>
-            <div className="relative mb-4">
+            <div  className="relative mb-4">
+              <label
+                htmlFor="name"
+                className="leading-7 text-sm  dark:text-white"
+              >
+                Seu nome
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder='Seu nome'
+                required
+                className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+                data-ddg-inputtype="identities.emailAddress"
+                data-ddg-autofill="true"
+                style={{
+                  backgroundSize: "auto 26px",
+                  backgroundPosition: "right center",
+                  backgroundRepeat: "no-repeat",
+                  backgroundOrigin: "content-box",
+                  transition: "background 0s ease 0s",
+                }}
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div  className="relative mb-4">
               <label
                 htmlFor="email"
                 className="leading-7 text-sm  dark:text-white"
@@ -34,6 +89,7 @@ const Message: React.FC = () => {
                 id="email"
                 name="email"
                 placeholder='seuemail@exemplo.com'
+                required
                 className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
                 data-ddg-inputtype="identities.emailAddress"
                 data-ddg-autofill="true"
@@ -46,6 +102,8 @@ const Message: React.FC = () => {
                     'url("chrome-extension://bkdgflcldnnnapblkhphbgpggdiikppg/img/logo-small-grayscale.svg")',
                   transition: "background 0s ease 0s",
                 }}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </div>
             <div className="relative mb-4">
@@ -59,7 +117,10 @@ const Message: React.FC = () => {
                 id="message"
                 name="message"
                 placeholder='Sua mensagem.'
+                required
                 className="w-full bg-white rounded border border-gray-300 focus:border-red-500 focus:ring-2 focus:ring-red-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
               ></textarea>
             </div>
             <button className="text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded text-lg">
@@ -68,7 +129,7 @@ const Message: React.FC = () => {
             <p className="text-xs text-gray-500 mt-3">
               Cheque a aba spam em seu e-mail caso não houver uma resposta em até 7 dias, caso o contrario entre em contato com o suporte | +55 (71) 99386-0508
             </p>
-          </div>
+          </form>
         </div>
       </section>
     </>
