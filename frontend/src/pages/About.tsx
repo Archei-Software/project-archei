ae620b519e21076552137ea2651ffbc001c8ff87
Victor Alves <victormssalves@outlook.com>
2023-05-16 17:55:53 -0300

    aaaaaa

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -2,12 +2,14 @@ import Header from "../components/header/Header";
 import Footer from "../components/footer/Footer";
 import usePageTitle from "../hooks/UsePageTitle";
 import AboutTeam from "../components/aboutTeam/AboutTeam";
+import AboutArchei from "../components/aboutArchei/AboutArchei"
 
 const About = () => {
   return (
     <>
       {usePageTitle("Sobre a Archei | Archei Software")}
       <Header />
+      <AboutArchei />
       <AboutTeam />
       <Footer />
     </>
