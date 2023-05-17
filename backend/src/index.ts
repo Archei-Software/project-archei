ab317c9b25667fc3c64a29fbe9dd459002ec3320
Victor Alves <victormssalves@outlook.com>
2023-05-17 19:56:19 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,12 @@
 import { server } from './server/Server';
 import cors from 'cors';
 
-server.use(cors())
+const corsOptions = {
+    origin: ['http://localhost:5173', 'https://archei.vercel.app/'],
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type,Authorization',
+  };
+  
+server.use(cors(corsOptions));
 
 server.listen(3000, () => console.log('Server is running port 3000'));
