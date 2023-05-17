dd7fcdbaf3c9149c76994012425a2a5418fe1485
Victor Alves <victormssalves@outlook.com>
2023-05-17 19:46:43 -0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,9 @@
 import { server } from './server/Server';
+import cors from 'cors';
+
+server.use(cors({
+    origin: '*'
+  }));
+  
 
 server.listen(3000, () => console.log('Server is running port 3000'));
